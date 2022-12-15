edc720fd8dea48167c492f1ce9746434dcd7b4af
roko556 <119864110+roko556@users.noreply.github.com>
2022-12-15 22:26:32 +0100

    adding extra key "weaknesses" for pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 let pokemonList= [
-    { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04 }, 
-    { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04 },
-    { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8 },
-    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0 }
+    { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04, weaknesses: 'water,ground,rock' }, 
+    { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04, weaknesses: 'fire,psychic,flying,ice' },
+    { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8, weaknesses: 'electric' },
+    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0,weaknesses:'water,ground,rock' }
 ]
