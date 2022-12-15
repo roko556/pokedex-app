02b98353d353c0bc4b060ac8a45fd6b2bf25c988
roko556 <119864110+roko556@users.noreply.github.com>
2022-12-15 22:13:08 +0100

    creating pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
 
+let pokemonList= [
+    { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04 }, 
+    { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04 },
+    { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8 },
+    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0 }
+]
