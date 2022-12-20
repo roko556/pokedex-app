7e3f61b36052f18eb850ad05809385c3d6a0eba6
roko556 <119864110+roko556@users.noreply.github.com>
2022-12-20 18:43:09 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 
+//adding an array with four different pokemon and their information 
 let pokemonList= [
     { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04, weaknesses: 'water,ground,rock' }, 
     { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04, weaknesses: 'fire,psychic,flying,ice' },
     { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8, weaknesses: 'electric' },
     { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0,weaknesses:'water,ground,rock' }
 ]
+
