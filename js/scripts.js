93379b0773d01e4dd2c63a04988d3ea246f3fb91
roko556 <119864110+roko556@users.noreply.github.com>
2022-12-20 19:26:56 +0100

    new pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ let pokemonList= [
     { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04, weaknesses: 'water,ground,rock' }, 
     { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04, weaknesses: 'fire,psychic,flying,ice' },
     { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8, weaknesses: 'electric' },
-    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0,weaknesses:'water,ground,rock' }
+    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0,weaknesses:'water,ground,rock' },
+    { name: 'Snorlax', type: 'normal', weight:1014, height: 6.11,weaknesses:'fighting' }
+    
 ]
 //creating loop that iterates over each item
 for (let i = 0;
     i < pokemonList.length; i++) {
     if(pokemonList[i].height >= 3) {
-        document.write(pokemonList[i].name + " height: " + pokemonList[i].height + "wow, that's big" + "<br>")
+        document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " wow, that's big" + "<br>")
     }
     else if (pokemonList[i].height >= 1 && pokemonList[i].height <3){
         document.write(pokemonList[i].name + " height: " + pokemonList[i].height+ "<br>" )
