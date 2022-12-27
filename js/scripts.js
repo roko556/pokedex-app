0ce7ab72802b9015e980e2376787f581bfdb47ca
roko556 <119864110+roko556@users.noreply.github.com>
2022-12-27 15:45:31 +0100

    changes for task 1.5-using for each function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,55 @@
+let pokemonRepository = (function () {
 
-//adding an array with four different pokemon and their information 
-let pokemonList= [
-    { name: 'Pikachu', type: 'electric', weight: 13.2, height: 1.04, weaknesses: 'water,ground,rock' }, 
-    { name: 'Bulbasaur', type: 'grass', weight: 15.2, height: 2.04, weaknesses: 'fire,psychic,flying,ice' },
-    { name: 'Squirtle', type: 'water', weight:19.8, height: 1.8, weaknesses: 'electric' },
-    { name: 'Charmander', type: 'fire', weight:18.8, height: 2.0,weaknesses:'water,ground,rock' },
-    { name: 'Snorlax', type: 'normal', weight:1014, height: 6.11,weaknesses:'fighting' }
-    
-]
-//creating loop that iterates over each item
-for (let i = 0;
-    i < pokemonList.length; i++) {
-    if(pokemonList[i].height >= 3) {
-        document.write(pokemonList[i].name + " height: " + pokemonList[i].height + "- wow, that's big" + "<br>")
-    }
-    else if (pokemonList[i].height >= 1 && pokemonList[i].height <3){
-        document.write(pokemonList[i].name + " height: " + pokemonList[i].height+ "<br>" )
-    }
-    else {
-        document.write(pokemonList[i].name + " height: " + pokemonList[i].height+ "<br>" )
-    }
+	let pokemonList = [];
+
+	function add (pokemon) {
+		if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.type && Object.keys(pokemon).length === 3) {
+			pokemonList.push(pokemon);
+		} else {
+			return `${pokemon} is not a Pokémon. Pokémon must be an object with the keys name, height and type`;
+		}
+	}
+
+	function getAll () {
+		return pokemonList
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+
+	};
+
+}) ();
+
+pokemonRepository.add({ name: 'Pikachu', height: 1.04, type:[  'electric'] } );
+pokemonRepository.add({ name: 'Snorlax', height: 6.11, type: ['normal'] });
+pokemonRepository.add({ name: 'Squirtle', height: 1.8, type: ['water'] });
+pokemonRepository.add({ name: 'Bulbasaur', height: 2.04, type: ['grass'] });
+pokemonRepository.add({ name: 'Charmander', height: 2.0, type: ['fire'] });
+
+let pokemonList = pokemonRepository.getAll();
+
+
+//forEach() function to iterate over the Pokémon in the pokemonList array
+pokemonList.forEach(printDetails);
+function printDetails (pokemon) {
+	let highlight = '';
+	if (pokemon.height > 5.0) {
+		highlight = " - Wow, that\’s big!";
+	}
+	document.write(`<li>${pokemon.name}(type: ${pokemon.type}) ${highlight}</li>`);
 }
+
+ let pokemonNames = [];
+ for (let i = 0; i < pokemonList.length; i++) {
+   pokemonNames.push(pokemonList[i].name);
+ }
+ function findPokemons (query) {
+	return pokemonNames.filter(function (name) {
+		return name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+	});
+}
+
+console.log(findPokemons('Iv'))
+	
